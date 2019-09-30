6095a80ac65fd61778444438c04fdaf81c430c4f
Phạm Ngọc Hòa <thaycacac@gmail.com>
2019-09-30 17:05:18 +0700

    update: formatted

--- thaycacac.js
+++ thaycacac.js
@@ -1,8 +1,8 @@
 const elements = ['.video-js .vjs-slider', '.assignfeedback_editpdf_widget', '.btn,#page-grade-grading-manage .actions .action,#rubric-rubric.gradingform_rubric #rubric-criteria .criterion .addlevel input,#rubric-rubric.gradingform_rubric .addcriterion', '.custom-control-label::before', '.switch input + label'];
 
-for(let i = 0; i < elements.length; i++){
-	let element = document.querySelectorAll(elements[i]);
-    for (let j = 0; j < element.length; j++) {
-		element[j].style.userSelect = "text";
-	}
+for(let i = 0; i < elements.length; i++) {
+  let element = document.querySelectorAll(elements[i]);
+  for (let j = 0; j < element.length; j++) {
+    element[j].style.userSelect = "text";
+  }
 }
